e68ff312a35793ced0cec33b8451b6e4c83d887d
ehdgus2502 <ehdgus2502@naver.com>
2024-06-07 19:36:45 +0900

    studyGuide

--- lib/user/firebase_settings.dart
+++ lib/user/firebase_settings.dart
@@ -3,12 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 class MyFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-      apiKey: "AIzaSyD475VCIuD3Y8erzAFuW3h5TR7GUUq7QGs",
+      apiKey: "AIzaSyAJluQjgf1AACZ0qcAtdYiCWBNalS2I5P0",
       authDomain: "studyguide_flutter.firebaseapp.com",
       projectId: "studyguide-flutter",
       storageBucket: "studyguide-flutter.appspot.com",
-      messagingSenderId: "884500961562",
-      appId: "1:884500961562:android:b9e1b079afa133e1ba74a3",
+      messagingSenderId: "54914006362",
+      appId: "1:54914006362:android:175fdc53de309828828763",
     );
   }
 }
