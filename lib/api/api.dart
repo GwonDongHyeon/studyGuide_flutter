c7af3f292edf49d65c881531511f2cf529d0cd9d
ehdgus2502 <ehdgus2502@naver.com>
2024-06-09 22:53:04 +0900

    studyGuide

---
+++ lib/api/api.dart
@@ -0,0 +1,8 @@
+class API {
+  static const hostConnect = "http://172.21.42.125/api_urls";
+
+  static const input = "$hostConnect/user/input.php";
+  static const output = "$hostConnect/user/output.php";
+  static const delete = "$hostConnect/user/delete.php";
+  static const update = "$hostConnect/user/update.php";
+}
